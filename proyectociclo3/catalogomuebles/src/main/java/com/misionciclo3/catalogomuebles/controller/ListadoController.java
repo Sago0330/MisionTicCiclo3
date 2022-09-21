f61de106a00faa223d048c87374c87dba21e30ec
Sago0330 <nilson.sago@gmail.com>
2022-09-20 21:27:32 -0500

    5 commit
    Montaje de la interfaz de usuario en html

--- proyectociclo3/catalogomuebles/src/main/java/com/misionciclo3/catalogomuebles/controller/ListadoController.java
+++ proyectociclo3/catalogomuebles/src/main/java/com/misionciclo3/catalogomuebles/controller/ListadoController.java
@@ -1,3 +1,13 @@
+package com.misionciclo3.catalogomuebles.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ListadoController {
 
+    @RequestMapping("/")
+    public String listarCatalogo() {
+        return "listado";
+    }
 }
